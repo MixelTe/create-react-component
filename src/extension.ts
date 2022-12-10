f75bf2e1a6cd5263fdae79bdd80aa3bbb321339d
Mike <63655075+MixelTe@users.noreply.github.com>
2022-12-10 14:42:42 +0300

    v1

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,50 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+import { TextEncoder } from 'util';
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export function activate(context: vscode.ExtensionContext)
+{
+	let disposable = vscode.commands.registerCommand('create-react-component.createComponent', (folder: vscode.Uri) =>
+	{
+		if (!folder)
+		{
+			vscode.window.showInformationMessage("Can't create here");
+			return
+		}
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "create-react-component" is now active!');
+		vscode.window.showInputBox({
+			title: "Name of component",
+			prompt: "Enter a name of the new component",
+			ignoreFocusOut: true,
+			placeHolder: "NewComponent",
+		}).then(name =>
+		{
+			try {
+				name = name || "NewComponent";
+				const wsedit = new vscode.WorkspaceEdit();
+				const config = vscode.workspace.getConfiguration('create-react-component');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('create-react-component.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Create React Component!');
+				const jsxName = config.get("jsxFileName", "index.jsx").replace(/\%name\%/g, name);
+				const cssName = config.get("cssFileName", "styles.module.css").replace(/\%name\%/g, name);
+				const jsx = config.get("jsxComponentContent", "").replace(/\%name\%/g, name);
+				const css = config.get("cssComponentContent", "").replace(/\%name\%/g, name);
+
+				const filePathjs = vscode.Uri.file(folder.path + `/${name}/` + jsxName);
+				const filePathcss = vscode.Uri.file(folder.path + `/${name}/` + cssName);
+
+				var enc = new TextEncoder();
+				wsedit.createFile(filePathjs, { ignoreIfExists: true, contents: enc.encode(jsx) });
+				wsedit.createFile(filePathcss, { ignoreIfExists: true, contents: enc.encode(css) });
+
+				vscode.workspace.applyEdit(wsedit);
+
+			} catch (e)
+			{
+				console.log(e);
+			}
+		});
 	});
 
 	context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
